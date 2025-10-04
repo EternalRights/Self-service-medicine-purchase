3a9055db1156a16a1eef4178fef9417d2fc0906d
EternalRights <3147268827@qq.com>
2025-10-04 20:57:03 +0800

    前端代码整体框架
    前端代码的目录设置 + 前端代码与Vue的结合 + 前端环境准备工作

---
+++ front-end-code/dev-code/src/App.vue
@@ -0,0 +1,15 @@
+<!-- 定义JS，控制模板部分的数据和行为 -->
+<script setup>
+//引入views/layout/index.vue命名为Layout
+import Layout from "@/views/layout/index.vue";
+</script>
+
+<!-- 模板部分，控制页面的结构 -->
+<template>
+  <Layout></Layout>
+</template>
+
+<!-- 样式部分，控制页面的样式 -->
+<style scoped>
+
+</style>
